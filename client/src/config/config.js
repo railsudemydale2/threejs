e2efa9b568a92a198e20ee0b809104a620f63a5c
dale <stewartdale45@yahoo.com>
2023-04-02 20:03:44 -0400

    final commit

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,9 +2,9 @@ const config = {
   development: {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
-  production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
-  },
+  // production: {
+  //   backendUrl: "https://ai-powered-3d.onrender.com",
+  // },
 };
 
 export default config;
