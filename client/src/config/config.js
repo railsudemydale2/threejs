8dbfaff02f72e344b9c0eae2cc437260f162c00c
dale <stewartdale45@yahoo.com>
2023-04-02 20:47:39 -0400

    final commit

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://threejs-products-app.com/api/v1/dalle",
+    backendUrl: "https://ai-powered-3d.onrender.com/api/v1/dalle",
   },
 };
 
